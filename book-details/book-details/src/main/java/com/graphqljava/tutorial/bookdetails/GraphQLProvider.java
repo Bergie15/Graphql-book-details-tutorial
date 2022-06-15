792a18438022dda16990c3d56d63693524563bd7
Bergie15 <Bergie.Bergstrom@gmail.com>
2022-06-15 12:59:36 -0500

    build schema and build wiring

--- book-details/book-details/src/main/java/com/graphqljava/tutorial/bookdetails/GraphQLProvider.java
+++ book-details/book-details/src/main/java/com/graphqljava/tutorial/bookdetails/GraphQLProvider.java
@@ -1,6 +1,9 @@
 @Component
 public class GraphQLProvider {
 
+	@Autowired
+	GraphQLDataFetchers graphQLDataFetchers;
+	
     private GraphQL graphQL;
 
     @Bean
@@ -17,6 +20,18 @@ public class GraphQLProvider {
     }
 
     private GraphQLSchema buildSchema(String sdl) {
-      // TODO: we will create the schema here later
+        TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(sdl);
+        RuntimeWiring runtimeWiring = buildWiring();
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        return schemaGenerator.makeExecutableSchema(typeRegistry, runtimeWiring);
+    }
+
+    private RuntimeWiring buildWiring() {
+        return RuntimeWiring.newRuntimeWiring()
+                .type(newTypeWiring("Query")
+                        .dataFetcher("bookById", graphQLDataFetchers.getBookByIdDataFetcher()))
+                .type(newTypeWiring("Book")
+                        .dataFetcher("author", graphQLDataFetchers.getAuthorDataFetcher()))
+                .build();
     }
 }
